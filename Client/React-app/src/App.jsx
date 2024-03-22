a131566aaa473c142bd2012a8384bd09beb651d2
vedha <vedh1305@gmail.com>
2024-03-22 15:37:32 +0530

    Redux Loader

--- Client/React-app/src/App.jsx
+++ Client/React-app/src/App.jsx
@@ -5,18 +5,29 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { Routes, Route } from "react-router-dom";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import MainPage from "./Components/UserDashboard/MainPage";
+import { useSelector } from "react-redux";
+import { HashLoader } from "react-spinners";
 
 function App() {
+  const { loading } = useSelector((state) => state.loader);
+
   return (
     <>
+    {loading && (
+            <div className="spinner flex justify-center items-center h-screen">
+            <HashLoader color="#36d7b7" className="h-16" />
+          </div>
+    )}
+
+
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Main" element={<MainPage/>}/>
+        <Route path="/Main" element={<MainPage />} />
       </Routes>
     </>
   );
--- Client/React-app/src/main.jsx
+++ Client/React-app/src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/Store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </HashRouter>
 );
